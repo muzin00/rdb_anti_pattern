540ac5d76bac581668b3e67a3ba36dfcf2395df7
yusuke <muzin00@gmail.com>
2024-02-25 18:20:44 +0900

    Add anti pattern hoge model

--- chapter_1/models.py
+++ chapter_1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class BadHoge(models.Model):
+    delete_falg = models.IntegerField(default=0, null=True, verbose_name="削除フラグ")
+
+    class Meta:
+        db_table = "bad_hoge"
